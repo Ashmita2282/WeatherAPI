4df99e17b1d21f834c6267cf923393a227c2060b
Asshu01 <shrivasashmita@gmail.com>
2024-01-27 20:26:39 +0530

    waether.js optimise

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,50 +3,44 @@ import "./Weather.css";
 
 export default function Weather() {
   const [city, setCity] = useState(null);
-  const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    const fetchApi = async () => {
-      try {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=2806c8f223266044603bdc8e864e6cad`;
-        const response = await fetch(url);
-        const resJson = await response.json();
-        console.log(resJson);
-        setCity(resJson);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    if (search) {
-      fetchApi();
+  const fetchApi = async (search) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=2806c8f223266044603bdc8e864e6cad`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      console.log(resJson);
+      setCity(resJson);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-  }, [search]);
+  };
+
 
   return (
     <div>
-    <h1 className="h1">Weather App</h1>
+      <h1 className="h1">Weather App</h1>
       <div id="container">
-      
         <i className="fa fa-map-marker" aria-hidden="true"></i>
         <input
           placeholder="Type city Name"
           type="search"
           className="inputFeild"
-          onChange={(event) => {
-            setSearch(event.target.value);
+          onKeyUp={(event) => {
+            if (event.key === "Enter") {
+              fetchApi(event.target.value);
+            }
           }}
-          
         />
-        <br/>
+        <br />
         {!city ? (
           <div>
-          <br/>
-          <p>Type the city name to search the wether</p>
+            <br />
+            <p>Type the city name to search the wether</p>
           </div>
         ) : (
           <div>
-            <h1>{search}</h1>
+            <h1>{city.name}</h1>
             {city.main && (
               <>
                 <h2>{city.main.temp}</h2>
